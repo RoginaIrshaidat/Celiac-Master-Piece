53552eee8f49cbb8b81b776fcdcb32d9610ef587
RoginaIrshaidat <115452721+RoginaIrshaidat@users.noreply.github.com>
2023-03-11 15:12:26 -0800

    BackEnd

---
+++ MasterPiece/MasterPiece/Controllers/AspNetUsersController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MasterPiece.Models;
+
+namespace MasterPiece.Controllers
+{
+    [Authorize]
+    public class AspNetUsersController : Controller
+    {
+        private MasterPieceEntities db = new MasterPieceEntities();
+
+        // GET: AspNetUsers
+        public ActionResult Index()
+        {
+            int productsCount = db.Products.Count();
+            int categoriesCount = db.Categories.Count();
+            int clientsCount = db.AspNetUsers.Count();
+            int PaymentsCount = db.Transactions.Count();
+
+            ViewBag.productsCount = productsCount;
+            ViewBag.categoriesCount=categoriesCount;
+            ViewBag.clientsCount = clientsCount;
+            ViewBag.PaymentsCount = PaymentsCount;
+            return View();
+        }
+        public ActionResult Customer()
+        {
+            return View(db.AspNetUsers.ToList());
+        }
+
+
+        // GET: AspNetUsers/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUser aspNetUser = db.AspNetUsers.Find(id);
+            if (aspNetUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetUser);
+        }
+
+        // GET: AspNetUsers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AspNetUsers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] AspNetUser aspNetUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AspNetUsers.Add(aspNetUser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(aspNetUser);
+        }
+
+        // GET: AspNetUsers/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUser aspNetUser = db.AspNetUsers.Find(id);
+            if (aspNetUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetUser);
+        }
+
+        // POST: AspNetUsers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] AspNetUser aspNetUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aspNetUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aspNetUser);
+        }
+
+        // GET: AspNetUsers/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUser aspNetUser = db.AspNetUsers.Find(id);
+            if (aspNetUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetUser);
+        }
+
+        // POST: AspNetUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            AspNetUser aspNetUser = db.AspNetUsers.Find(id);
+            db.AspNetUsers.Remove(aspNetUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MasterPiece/MasterPiece/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using MasterPiece.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MasterPiece.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MasterPieceEntities db = new MasterPieceEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Shop()
+        {
+            return View(db.Products.ToList());
+        }
+        public ActionResult Product_Details()
+        {
+            return View(db.Products.ToList());
+        }
+    }
+}
